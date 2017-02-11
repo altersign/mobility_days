747d8bdcf6e071ce8284da0cd4b445fea502cefd
Yaroslav Polyakov <contact@altersign.com>
2017-02-11 11:20:06 +0100

    Show current coordinates in app

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -22,12 +22,19 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
+    // default coordinates are "Museum für Kommunikation Nürnberg" (Hackathon location)
+    latitude: '49.4461443',
+    longitude: '11.0744655',
+
+    locationService: 'GoogleLocator',
+
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        this.updateLocation();
     },
 
     // Update DOM on a Received Event
@@ -40,7 +47,54 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    updateLocation: function () {
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        var onSuccess = function(position) {
+            /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+                  'Longitude: '         + position.coords.longitude         + '\n' +
+                  'Altitude: '          + position.coords.altitude          + '\n' +
+                  'Accuracy: '          + position.coords.accuracy          + '\n' +
+                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                  'Heading: '           + position.coords.heading           + '\n' +
+                  'Speed: '             + position.coords.speed             + '\n' +
+                  'Timestamp: '         + position.timestamp                + '\n');*/
+            console.log('Received position: ');
+            console.log(position);
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+            this.renderLocation();
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        var onError = function (error) {
+            console.log('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+            this.renderLocation();
+
+        };
+
+        navigator.geolocation.getCurrentPosition(
+            onSuccess.bind(this),
+            onError.bind(this),
+            { timeout: 30000, enableHighAccuracy: true }
+        );
+    },
+
+    renderLocation: function () {
+            var parentElement = document.getElementById("current-location");
+            var latitudeElement = parentElement.querySelector('.latitude');
+            var longitudeElement = parentElement.querySelector('.longitude');
+
+            latitudeElement.textContent = this.latitude;
+            longitudeElement.textContent = this.longitude;
+            parentElement.setAttribute('style', 'display:block;');
     }
+
 };
 
 app.initialize();
